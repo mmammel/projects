19ade4ae2d6c3f1ccec744cea0aaf24e8da638b7
Max Mammel <max.mammel@gmail.com>
2020-09-03 23:37:59 -0400

    Add random stuff

--- C/lew/twoNotTouch/twonottouch.c
+++ C/lew/twoNotTouch/twonottouch.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 char blocks [10][10] ;
+char overrun [5];
 
 main(){
 int i;
@@ -8,5 +9,7 @@ for( i=0 ; i<10 ; i=i+1 ) scanf( "%s", &blocks[i][0] );
 for( i=0 ; i<10 ; i=i+1 ) printf( "%10s\n", &blocks[i][0] );
 */
 for( i=0 ; i<10 ; i=i+1 ) printf( "%c\n", blocks[i][0] );
-for( i=0 ; i<10 ; i=i+1 ) printf( "%.10s\n", &(blocks[i][0]) );
+for( i=0 ; i<10 ; i=i+1 ) printf( "%.8s\n", &(blocks[i][0]) );
+for( i=0 ; i<10 ; i=i+1 ) printf( "%s\n", &(blocks[i][0]) );
+printf("Overrun: %s\n", &(overrun[0]) );
 }
