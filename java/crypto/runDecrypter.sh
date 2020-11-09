402d8f212126b5dec2b1c3cd79bd50575844b0d4
Max Mammel <max.mammel@gmail.com>
2020-11-09 11:03:19 -0500

    Remove

--- java/crypto/runDecrypter.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_05.jdk/Contents/Home/bin
-
-if [ ! -f ./Encryption.class ]; then
-  $JAVA_HOME/javac -classpath ./jasypt-1.5.jar Encryption.java
-fi
-
-$JAVA_HOME/java -classpath './commons-codec-1.5.jar:./commons-lang-2.6.jar:./jasypt-1.5.jar:.' Encryption $1 $2
