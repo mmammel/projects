bcaf4f724bb7cdb25a251548f66cd5faae63085e
Max Mammel <max.mammel@gmail.com>
2020-11-10 01:00:33 -0500

    Add some more JSON stuff

--- java/JSONorg/JSONPointerTest.java
+++ java/JSONorg/JSONPointerTest.java
@@ -1,4 +1,5 @@
 import org.json.JSONObject;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONPointer;
 import java.util.*;
@@ -43,6 +44,43 @@ public class JSONPointerTest
     JSONObject j = new JSONObject( JSON_STRING );
     JSONPointer jp = new JSONPointer( args[0] );
     Object result = jp.queryFrom( j );
+    System.out.println( result.getClass().getName() );
+    if( result.getClass() == java.lang.String.class ) {
+      System.out.println( "It's a string!" );
+    } else if( result.getClass() == org.json.JSONArray.class ) {
+      System.out.println( "It's an array!" );
+    }
     System.out.println( result.toString() );
+
+    System.out.println( "Get a list: " );
+    List<String> aslist = evaluateToList( args[0], j );
+    for( String s : aslist ) {
+      System.out.println(s);
+    }
+    System.out.println( "...done!");
+  }
+
+  public static List<String> evaluateToList( String path, JSONObject j ) {
+    List<String> retVal = new ArrayList<String>();
+    JSONPointer p = new JSONPointer( path );
+    Object result = p.queryFrom( j );
+
+    if( result != null ) {
+      if( result.getClass() == java.lang.String.class ) {
+        retVal.add( result.toString() );
+      } else if ( result.getClass() == org.json.JSONArray.class ) {
+        Object temp = null;
+        JSONArray arr = (JSONArray)result;
+        for( int i = 0; i < arr.length(); i++ ) {
+          temp = arr.get( i );
+          if( temp != null && temp.getClass() == java.lang.String.class ) {
+            retVal.add( temp.toString() );
+          }
+        }
+      }
+    }
+
+    return retVal;
+    
   }
 }
--- java/JSONorg/JSONPointerUtil.java
+++ java/JSONorg/JSONPointerUtil.java
@@ -1,3 +1,16 @@
 public class JSONPointerUtil {
+
+  public static void main( String [] args ) {
+  }
+
+  public static List<String> evaluate( String path, JSONObject json ) {
+
+    int starIdx = 0;
+    if( (starIdx = path.indexOf('*')) > 0 ) {
+      Object result = null;
+
+      
+    }
+  }
   
 }
