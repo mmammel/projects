67422f17af75e9e50231f5c61942a59408e6e945
Max Mammel <max.mammel@gmail.com>
2020-12-30 13:07:09 -0500

    Fetch live answers from the current online puzzle

--- scripts/php/spellingBee/sb.php
+++ scripts/php/spellingBee/sb.php
@@ -12,7 +12,9 @@ if( isset($_GET["s"]) ) {
   } else {
     echo "error";
   }
-
+} else if( isset($_GET["t"]) ) {
+  $result = shell_exec('curl -s https://www.nytimes.com/puzzles/spelling-bee | awk '.escapeshellarg('{ idx=index($0,"window.gameData"); if(idx>0) { obj=substr($0,idx); idx=index(obj, "{"); obj=substr(obj, idx); idx=index(obj,"</script>"); obj=substr(obj,0,idx-1); printf("%s\n", obj); } }') );
+  echo $result;
 }
 
 
