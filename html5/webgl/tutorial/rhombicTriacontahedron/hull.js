48d7477744ba6333a7b1aa6c67c6f47068831f33
mmammel <max.mammel@gmail.com>
2026-01-15 16:35:43 -0500

    feat(hull): Scaffold hull.js and include it in index.html

---
+++ html5/webgl/tutorial/rhombicTriacontahedron/hull.js
@@ -0,0 +1,18 @@
+/**
+ * 3D QuickHull Algorithm Implementation
+ * 
+ * Target Complexity: O(n log n)
+ */
+
+var QuickHull = {
+  /**
+   * Finds the convex hull of a set of 3D points.
+   * @param {Array<number>} vertices - Flat array of vertex coordinates [x0, y0, z0, x1, y1, z1, ...]
+   * @returns {Array<Array<number>>} - Array of face indices, each being an array of 3 indices.
+   */
+  getHull: function(vertices) {
+    // TODO: Implement QuickHull algorithm
+    console.log("QuickHull.getHull called with", vertices.length / 3, "points");
+    return [];
+  }
+};
